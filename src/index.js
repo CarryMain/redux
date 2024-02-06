21bf19b4fe48fffb98aa65568d46586a927d50c5
CarryMain <mr.turanov98@gmail.com>
2024-02-06 10:29:18 +0300

    Provider in working

--- src/index.js
+++ src/index.js
@@ -2,34 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer)
 
-const {dispatch, subscribe, getState} = store 
 
-const update = () => {
-  document.getElementById('counter').textContent = getState().value
-}
-
-subscribe(update)
-
-const {inc,dec,rnd} = bindActionCreators(actions , dispatch)
-
-
-document.getElementById('inc').addEventListener('click', inc)
-document.getElementById('dec').addEventListener('click', dec)
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10)
-  rnd(value)
-})
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
   <React.StrictMode>
-    <>
-
-    </>
+      <Provider store={store}>
+        <App/>
+      </Provider>
   </React.StrictMode>
 );
