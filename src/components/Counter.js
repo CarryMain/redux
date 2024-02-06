e26f90ecc9b2bbfd2f7d9404d47d38fb7e949722
CarryMain <mr.turanov98@gmail.com>
2024-02-06 12:45:19 +0300

    connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,5 @@
+import { connect } from "react-redux"
+import * as actions from '../actions';
 
 const Counter = ({counter, inc, dec, rnd}) => {
     return ( 
@@ -10,4 +12,14 @@ const Counter = ({counter, inc, dec, rnd}) => {
     )
 }
 
-export default Counter 
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value,
+    }
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators(actions, dispatch)
+// }
+
+export default connect(mapStateToProps, actions)(Counter) 
